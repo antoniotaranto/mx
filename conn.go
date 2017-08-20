8a0bb06716a7f7d60673567622ed1d34c070eecc
Dmitry Sedykh <sedykh@gmail.com>
2017-08-20 22:51:37 +0300

    disable keepAlive log

--- conn.go
+++ conn.go
@@ -259,7 +259,7 @@ func (c *Conn) sendKeepAlive() {
 		c.mu.Lock()
 		c.keepAlive.Reset(KeepAliveDuration)
 		c.mu.Unlock()
-		csta(false, 0, []byte("<keepalive/>"))
+		// csta(false, 0, []byte("<keepalive/>"))
 	}
 }
 
